dc845873eb9d2dd4dd7eb12ad0094daf2ab9c7ef
NotRyken <notryken@gmail.com>
2023-12-11 13:35:38 +0800

    Backport to mc1.20.2

--- common/src/main/java/notryken/commandkeys/gui/component/listwidget/ConfigListWidgetMono.java
+++ common/src/main/java/notryken/commandkeys/gui/component/listwidget/ConfigListWidgetMono.java
@@ -17,9 +17,9 @@ public class ConfigListWidgetMono extends ConfigListWidget {
 
     public MsgKeyMapping selectedMsgKey;
 
-    public ConfigListWidgetMono(Minecraft client, int width, int height, int y,
+    public ConfigListWidgetMono(Minecraft client, int width, int height, int top, int bottom,
                                 int itemHeight, Screen parentScreen, Component title) {
-        super(client, width, height, y, itemHeight, parentScreen, title);
+        super(client, width, height, top, bottom, itemHeight, parentScreen, title);
 
         addEntry(new Entry.SettingToggles(width, this));
 
@@ -80,22 +80,22 @@ public class ConfigListWidgetMono extends ConfigListWidget {
     @Override
     public void reloadScreen() {
         ConfigListWidget listWidget = new ConfigListWidgetMono(
-                client, width, height, y, itemHeight, parentScreen, screenTitle);
+                client, width, height, top, bottom, itemHeight, parentScreen, screenTitle);
         listWidget.setScrollAmount(this.getScrollAmount());
         client.setScreen(new ConfigScreenMono(parentScreen, client.options, screenTitle, listWidget));
     }
 
     @Override
-    public ConfigListWidgetMono resize(int width, int height, int y) {
+    public ConfigListWidgetMono resize(int width, int height, int top, int bottom) {
         ConfigListWidgetMono listWidget = new ConfigListWidgetMono(
-                client, width, height, y, itemHeight, parentScreen, screenTitle);
+                client, width, height, top, bottom, itemHeight, parentScreen, screenTitle);
         listWidget.setScrollAmount(getScrollAmount());
         return listWidget;
     }
 
     private void openDualConfigScreen() {
         ConfigListWidget listWidget = new ConfigListWidgetDual(
-                client, width, height, y, itemHeight, parentScreen, screenTitle);
+                client, width, height, top, bottom, itemHeight, parentScreen, screenTitle);
         client.setScreen(new ConfigScreenMono(parentScreen, client.options, screenTitle, listWidget));
     }
 
