70e0177f335da7f652cb4e8f91abd45f4f1a502b
NotRyken <127091011+NotRyken@users.noreply.github.com>
2025-09-18 08:29:34 +0800

    Update to mc1.21.4

--- common/src/main/java/dev/terminalmc/commandkeys/gui/widget/list/OptionList.java
+++ common/src/main/java/dev/terminalmc/commandkeys/gui/widget/list/OptionList.java
@@ -111,7 +111,7 @@ public abstract class OptionList extends ContainerObjectSelectionList<OptionList
      * Initializes the {@link OptionList}.
      */
     protected void init() {
-        double scrollAmount = getScrollAmount();
+        double scrollAmount = scrollAmount();
 
         clearEntries();
         setFocused(null);
@@ -167,7 +167,7 @@ public abstract class OptionList extends ContainerObjectSelectionList<OptionList
     }
 
     @Override
-    protected int getScrollbarPosition() {
+    protected int scrollBarX() {
         return width / 2 + rowWidth / 2;
     }
 
